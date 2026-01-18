4d93ba427b0cf1f365d036162e4ec63c5ab30137
sogik <itssogik@gmail.com>
2026-01-18 01:01:55 +0100

    Remove empty methods and Unity default comments from PatrolState, Potion, Keypad; remove unused System.Linq import from PlayerAttack

--- Assets/Scripts/Shop/Potion.cs
+++ Assets/Scripts/Shop/Potion.cs
@@ -3,18 +3,6 @@ using UnityEngine;
 public class Potion : Interactable
 {
     public float healAmount = 25f;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-    
 
     protected override void Interact()
     {
